b1dd505c9512e7ae9de4b3a897b5d5cebb275508
uttamraj9 <uttamraj9@gmail.com>
2023-04-13 15:52:53 +0100

    updated location in hdfs

--- src/main/scala/kafkaspark/ReadFromKafka.scala
+++ src/main/scala/kafkaspark/ReadFromKafka.scala
@@ -40,7 +40,7 @@ object ReadFromKafka {
     val df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "ip-172-31-3-80.eu-west-2.compute.internal:9092").option("subscribe", topic).option("startingOffsets", "earliest").load().select(from_json(col("value").cast("string"), schema).as("data")).selectExpr("data.*")
 
     // Write the DataFrame as CSV files to HDFS
-    df.writeStream.format("parquet").option("checkpointLocation", "/tmp/jenkins/kafka/trainarrival/checkpoint").option("path", "/tmp/jenkins/kafka/trainarrival/data").start().awaitTermination()
+    df.writeStream.format("csv").option("checkpointLocation", "/tmp/jenkins/kafka/trainarrival/checkpoint").option("path", "/tmp/jenkins/kafka/trainarrival/data").start().awaitTermination()
 
   }
 
