c44b7f8c297ccccd86f1e18c34b5c5d3814588ef
uttamraj9 <uttamraj9@gmail.com>
2023-07-10 22:45:26 +0530

    updaed

--- src/main/scala/kafkaspark/SendDataToKafka.scala
+++ src/main/scala/kafkaspark/SendDataToKafka.scala
@@ -26,7 +26,7 @@ object SendDataToKafka {
       val topicSampleName: String = "arrivaldata"
 
       messageDF.selectExpr("CAST(id AS STRING) AS key", "to_json(struct(*)) AS value").selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)").write.format("kafka").option("kafka.bootstrap.servers", kafkaServer).option("topic", topicSampleName).save()
-
+println("message is loaded to kafka topic")
       Thread.sleep(10000) // wait for 10 seconds before making the next call
     }
   }
