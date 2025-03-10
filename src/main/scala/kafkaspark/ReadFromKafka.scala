f6c6069b0447892d692b667f4aefcc1d61d613df
uttamraj9 <uttamraj9@gmail.com>
2025-03-10 12:58:55 +0000

    Update ReadFromKafka.scala

--- src/main/scala/kafkaspark/ReadFromKafka.scala
+++ src/main/scala/kafkaspark/ReadFromKafka.scala
@@ -38,8 +38,7 @@ object ReadFromKafka {
     ))
 
     // Read the JSON messages from Kafka as a DataFrame
-    val df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", " ip-172-31-8-235.eu-west-2.compute.internal:9092,ip-172-31-14-3.eu-west-2.compute.internal:9092").option("subscribe", topic).option("assign", s"$topic,$partitionId,earliest").load().select(from_json(col("value").cast("string"), schema).as("data")).selectExpr("data.*")
-
+    val df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", " ip-172-31-8-235.eu-west-2.compute.internal:9092,ip-172-31-14-3.eu-west-2.compute.internal:9092").option("subscribe", topic).option("startingOffsets", "earliest").load().select(from_json(col("value").cast("string"), schema).as("data")).selectExpr("data.*")
     // Write the DataFrame as CSV files to HDFS
     df.writeStream.format("csv").option("checkpointLocation", "/tmp/jenkins/kafka/trainarrival/checkpoint").option("path", "/tmp/jenkins/kafka/trainarrival/data").start().awaitTermination()
 
