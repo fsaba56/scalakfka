bdfc6a8c999f413db5e8f5990557a8bca550e69f
fsaba56 <fsaba56@gmail.com>
2025-03-13 02:59:27 -1200

    Update ReadFromKafka.scala

--- src/main/scala/kafkaspark/ReadFromKafka.scala
+++ src/main/scala/kafkaspark/ReadFromKafka.scala
@@ -6,16 +6,6 @@ object ReadFromKafka {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("KafkaToJson").master("local[*]").getOrCreate()
 
-    // Define the Kafka parameters
-    val kafkaParams = Map[String, Object](
-      "bootstrap.servers" -> "ip-172-31-14-3.eu-west-2.compute.internal:9092",
-      "key.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
-      "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
-      "group.id" -> "group1",
-      "auto.offset.reset" -> "earliest",
-      "enable.auto.commit" -> (false: java.lang.Boolean)
-    )
-
     // Define the Kafka topic to subscribe to
     val topic = "tfl_underground_Details"
 
@@ -32,9 +22,8 @@ object ReadFromKafka {
     ))
 
     // Read the JSON messages from Kafka as a DataFrame
-    val df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", " ip-172-31-8-235.eu-west-2.compute.internal:9092,ip-172-31-14-3.eu-west-2.compute.internal:9092").option("subscribe", topic).option("startingOffsets", "latest").option("failOnDataLoss", "false").load().select(from_json(col("value").cast("string"), schema).as("data")).selectExpr("data.*")
+    val df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", " ip-172-31-8-235.eu-west-2.compute.internal:9092,ip-172-31-14-3.eu-west-2.compute.internal:9092").option("subscribe", topic).option("startingOffsets", "earliest").option("failOnDataLoss", "false").load().select(from_json(col("value").cast("string"), schema).as("data")).selectExpr("data.*")
     // Write the DataFrame as CSV files to HDFS
     df.writeStream.format("csv").option("checkpointLocation", "/tmp/jenkins/kafka/trainarrival/checkpoint").option("path", "/tmp/jenkins/kafka/trainarrival/data").start().awaitTermination()
   }
-
 }
