02373b1cb371ceec0bc8d2196ab9dc13f3ece538
fsaba56 <fsaba56@gmail.com>
2025-03-13 01:37:52 -1200

    Update SendDataToKafka.scala

--- src/main/scala/kafkaspark/SendDataToKafka.scala
+++ src/main/scala/kafkaspark/SendDataToKafka.scala
@@ -11,16 +11,23 @@ object SendDataToKafka {
       .getOrCreate()
     while (true) {
       import spark.implicits._
-      val apiUrl = "https://api.tfl.gov.uk/Line/victoria/Arrivals?app_id=92293faa428041caad3dd647d39753a0&app_key=ba72936a3db54b4ba5792dc8f7acc043"
+      val apiUrl = "https://api.tfl.gov.uk/Line/Mode/tube/Status?app_id=92293faa428041caad3dd647d39753a0&app_key=ba72936a3db54b4ba5792dc8f7acc043"
       val response = get(apiUrl, headers = headers)
       val total = response.text()
       val dfFromText = spark.read.json(Seq(total).toDS)
 
-      // select the columns you want to include in the message
-
-      val messageDF = dfFromText.select($"id", $"stationName", $"lineName", $"towards",
-        $"expectedArrival",$"vehicleId",$"platformName",$"direction",$"destinationName",
-        $"timestamp",$"timeToStation", $"currentLocation",$"timeToLive")
+      // Select the columns from the nested JSON
+      val messageDF = dfFromText.select(
+        $"id", 
+        $"name", 
+        $"modeName", 
+        $"lineStatuses.statusSeverityDescription".alias("lineStatus"),
+        $"lineStatuses.statusSeverity",
+        $"created",
+        $"modified",
+        $"serviceTypes.name".alias("serviceType"),
+        $"crowding"
+      )
 
       val kafkaServer: String = "ip-172-31-8-235.eu-west-2.compute.internal:9092,ip-172-31-14-3.eu-west-2.compute.internal:9092"
       val topicSampleName: String = "arrivaldata_New"
